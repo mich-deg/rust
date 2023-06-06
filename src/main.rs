9003a44c45e902a25bca34431f74ab0aedcde48d
Michael <lemichael1215@gmail.com>
2023-06-06 12:07:55 +0300

    added printing outputs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, world!");
+
+    println!("The value of the constant is {}", 10);
+
+    println!("First name {}, Last name {}", "John", "Doe");
+
+    println!("\n I love {2}, using {1} and {0}", "JavaScript", "Rust", "coding");
+
+    println!("{language} is a language empowering everyone to build reliable and efficient software", language="Rust")
+ 
 }
