4d8a9f4188af8ea714d792c33871c48e5c1b5e71
Michael <lemichael1215@gmail.com>
2023-06-07 13:46:17 +0300

    added functions and inputs

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,53 @@
 fn main() {
 
-   /* Tuples 
-        - Destructing tuples
-        - Nested tuples
-   */
+     basic_fn();
+     
+     function_with_inputs("John", 40_000);
 
-   let my_info = ("Salary", 40_000);
-   println!("{} is equal to {}", my_info.0, my_info.1);
+     let first_name = "Doe";
+     let salary_info = 50_000;
+     function_with_inputs(first_name, salary_info);
 
-   let (salary, salary_value) = my_info;
-   println!("The values of the individual variables are, {} and {}", &salary, salary_value);
+    let multiply =  function_io(10, 15);
+    println!("The answer of the multiplication is {}", multiply );
 
-   let nested_tuples = (4, 5.5, (3, 2.5), "Hello");
-   let element = nested_tuples.2.0;
-   println!("The value of element is {}", element);
+    let (multiplication, addition, subtraction) = function_input_with_multiple_output(10, 15);
+    println!("M = {}, Add = {}, Sub = {}", multiplication, addition, subtraction);
 
-   /* Arrays
-         - Updating elements
-         - Strings and char arrays
-         - Slices
-    */
+    /* Code Blocks */
 
-   let mut number_array = [4,5,6,8,9];
-   println!("{}", number_array[0]);
+    let full_name = {
+     let first_name = "John";
+     let last_name = "Doe";
+     format!("{} {}", first_name, last_name)
+    };
 
-   println!("{:?}", number_array);
+    println!("My full name is {}", full_name);
 
-   number_array[4] = 5;
-   println!("{:?}", number_array);
+    /* Inputs from user */
 
-   let string_array = ["apple", "orange", "grapes", "banana"];
-   println!("{:?}", string_array);
+    let mut n = String::new();
+    std::io::stdin()
+    .read_line(&mut n)
+    .expect("failed to read input");
+    
+    let n:f64 = n.trim().parse().expect("invalid input");
+    println!("{:?}", n);
 
-   let subset_array = &number_array[0..=3];
-   println!("The subset of the value of the array are {:?}", subset_array);
+}
 
-   println!("Elements in the array are {}", number_array.len());
+fn basic_fn(){
+     println!("This is a basic function");
+}
 
-   println!("The array is occupying {} bytes", std::mem::size_of_val(&number_array));
+fn function_with_inputs(name: &str, salary: i32){
+     println!("The name is {} and the salary is {}",name, salary);
+}
 
-   let check_index = number_array.get(2);
-   println!("{:?}", check_index);
+fn function_io(num1:i32, num2:i32) -> i32 {
+     num1 * num2
+}
 
-
-   /* Vectors */
-
-   let mut number_vec = vec![2,3,4,6,7,8,9,3,5,0, 10, 15, 38];
-   println!("{}", number_vec[0]);
-
-   number_vec[4] = 40;
-   println!("{:?}", number_vec);
-
-   let mut string_array_1 = vec!["apple", "orange", "grapes", "banana"];
-   string_array_1[0] = "John Doe";
-
-   let subset_vec = &number_vec[0..6];
-   println!("The subset of values of the vector are {:?}", subset_vec);
-
-   println!("Elements in the vector are {}", number_vec.len());
-
-   let check_index = number_vec.get(7);
-   println!("{:?}", check_index);
-
-   number_vec.push(50);
-   number_vec.push(90);
-
-   println!("The values of the vector are {:?}", number_vec);
-
-   number_vec.remove(5);
-   println!("The vector after removing the element at index 5 is {:?}", number_vec);
-
-   println!("The value of 3 exist in the vector {}", number_vec.contains(&3));
-
+fn function_input_with_multiple_output(num1:i32, num2:i32) -> (i32, i32, i32) {
+     (num1*num2, num1+num2, num1-num2)
 }
